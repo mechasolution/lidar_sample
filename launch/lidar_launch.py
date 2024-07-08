a1fcdf23503245a1a46b3aafef4a610c6196b0eb
dadayoung0 <dadayoung09@gmail.com>
2024-07-08 17:21:17 +0900

    init

---
+++ launch/lidar_launch.py
@@ -0,0 +1,29 @@
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+import os
+
+
+def generate_launch_description():
+    sample_file = os.path.join(get_package_share_directory("lidar_sample"), "sample")
+
+    lidar_sample = ExecuteProcess(
+        cmd=["ros2 bag play ", sample_file, " --loop"], shell=True
+    )
+
+    tf2_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="static_tf_pub_laser",
+        arguments=["0", "0", "0.02", "0", "0", "0", "1", "base_link", "laser_frame"],
+    )
+
+    return LaunchDescription(
+        [
+            lidar_sample,
+            tf2_node,
+        ]
+    )
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = "lidar_sample"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*.py")),
+        ),
+        (
+            os.path.join("share", package_name, "sample"),
+            glob(os.path.join("sample", "*")),
+        ),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Mechasolution",
+    maintainer_email="techms5499@gmail.com",
+    description="ROS 2 Package to play LiDAR sample",
+    license="Apache 2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
